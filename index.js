c8d6f662d189089f6dfc32eac4e34321c706ef3e
Amanda Ackerman <ackerm44@gmail.com>
2017-10-02 18:02:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
 
   if (string.toUpperCase() === uppercase) {
-    return (`YES INDEED`);
+    return (`YES INDEED!`);
   }  else if (string.toLowerCase() === lowercase) {
     return ('I can\'t hear you!')
   }  else if (string === "I love you, Grandma.") {
